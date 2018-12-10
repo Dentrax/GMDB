a0f1a6b10fc56f6268738e8e04510f4c59cb6531
Furkan Türkal <furkan.turkal@hotmail.com>
2018-12-10 10:23:19 +0300

    added service_test.go template

---
+++ services/imdb/service_test.go
@@ -0,0 +1,20 @@
+// ====================================================
+// GMDB Copyright(C) 2019 Furkan Türkal
+// This program comes with ABSOLUTELY NO WARRANTY; This is free software,
+// and you are welcome to redistribute it under certain conditions; See
+// file LICENSE, which is part of this source code package, for details.
+// ====================================================
+
+package imdb
+
+import (
+	"testing"
+)
+
+func TestGetMovie(t *testing.T) {
+
+}
+
+func TestGetDocumentForURL(t *testing.T) {
+
+}
