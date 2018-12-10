315c64290423b61f1539f95d03963fb94fbbefef
Furkan Türkal <furkan.turkal@hotmail.com>
2018-12-10 10:22:49 +0300

    added imdb.go

---
+++ services/imdb/imdb.go
@@ -0,0 +1,25 @@
+// ====================================================
+// GMDB Copyright(C) 2019 Furkan Türkal
+// This program comes with ABSOLUTELY NO WARRANTY; This is free software,
+// and you are welcome to redistribute it under certain conditions; See
+// file LICENSE, which is part of this source code package, for details.
+// ====================================================
+
+package imdb
+
+import (
+	"gmdb/models"
+)
+
+//func GetMovieByTitle(title string) (*models.Movie, error) {
+//	cl := New("")
+//	return cl.GetMovieByTitle(title)
+//}
+
+//TODO: Do something here?
+
+func GetMovieParentsGuide2(url string) (*models.ParentsGuide, error) {
+	//var cl *Service = New("IMDB", url)
+	//return cl.GetParentsGuide()
+	return nil, nil
+}
